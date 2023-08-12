fdfd7e2fa9a50dda5a4e257069cb7fcbec7dc15d
Ruwan <ruwanliyanagegalle1994@gmail.com>
2023-08-12 22:33:10 +0530

    Add init commit

---
+++ src/main/java/com/siyapatha/bookshop/siyapathacore/SiyapathaCoreApplication.java
@@ -0,0 +1,13 @@
+package com.siyapatha.bookshop.siyapathacore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiyapathaCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiyapathaCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siyapatha/bookshop/siyapathacore/SiyapathaCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siyapatha.bookshop.siyapathacore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiyapathaCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
